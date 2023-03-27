07d0b864bdf1ec14d4bdcde343a5caec457695f4
Chahak Bhartiya <119099545+cha1708@users.noreply.github.com>
2023-03-27 12:19:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = '8056943d56d7e2505f8c12fe6503240d';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body)
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    });
+  })
